9e5601323fa3ca7729403761124625e1fc3a5b51
Gabriel de Oliveira Ferreira <gabrieloferreira@hotmail.com>
2018-06-07 11:29:37 -0300

    CEDUP - 07/06

---
+++ Lista-ArrayList/src/ListaArrayList04.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+
+/**
+ * @author Gabriel de Oliveira Ferreira
+ */
+public class ListaArrayList04 {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+        }
+    }
+
+}
