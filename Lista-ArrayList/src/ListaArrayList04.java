e71e3a2b44fc5ec5077ed3dd02c3b1422e5136f9
Gabriel de Oliveira Ferreira <gabrieloferreira@hotmail.com>
2018-06-08 08:33:13 -0300

    CEDUP - 08/06 - 08:32
    Exercício 4 resolvido

--- Lista-ArrayList/src/ListaArrayList04.java
+++ Lista-ArrayList/src/ListaArrayList04.java
@@ -9,10 +9,29 @@ public class ListaArrayList04 {
     public static void main(String[] args) {
         ArrayList<Integer> numeros = new ArrayList<>();
         
-        for (int i = 0; i < args.length; i++) {
-            String arg = args[i];
+        for (int i = 0; i < 10; i++) {
+            numeros.add(i);
             
         }
+        System.out.println("Números");
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+        System.out.println("Números restantes: " + numeros.size());
+        
+        numeros.remove(3);
+        System.out.println("Números");
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+        System.out.println("Números restantes: " + numeros.size());
+        
+        numeros.remove(4);
+        System.out.println("Números");
+        for (int i = 0; i < numeros.size(); i++) {
+            System.out.println(numeros.get(i));
+        }
+        System.out.println("Números restantes: " + numeros.size());
     }
 
 }
